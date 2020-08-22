63fa494d2bddbcee74577e1585f91632e7e0d559
Alemasov Sergey Aleksandrovich <s.alemasov@tinkoff.ru>
2020-08-22 22:18:34 +0400

    added viewport scripts

--- src/scripts/bootstrap.js
+++ src/scripts/bootstrap.js
@@ -1,7 +1,19 @@
 (function (app) {
     function bootstrap() {
+        setViewport();
         app.header.init();
     }
 
+    function setViewport() {
+        var mvp = document.getElementById('viewport');
+
+        if (screen.width < 640) {
+            mvp.setAttribute('content', 'user-scalable=no, width=640');
+        } else {
+            mvp.setAttribute('content', 'user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1');
+        }
+    }
+
+    window.addEventListener('orientationchange', setViewport);
     window.onload = bootstrap;
 })(app);
